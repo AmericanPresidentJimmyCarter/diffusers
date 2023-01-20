43a9e7bb5880c593541c9f3caf93a9bb5754c737
Your Name <you@example.com>
2023-01-20 02:25:12 +0000

    WIP

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 import utils
 from PIL import Image, ImageOps, ImageChops
 
-model = StableDiffusionPipeline.from_pretrained('stabilityai/stable-diffusion-2-1-base')
+model = StableDiffusionPipeline.from_pretrained('sd_prior_model_base')
 from diffusers import EulerDiscreteScheduler
 
 THRESHOLD_FIRM = 190
@@ -19,8 +19,8 @@ model = model.to('cuda')
 
 prompt = "pikachu surfing a giant wave eating an ice cream cone"
 
-import debugpy
-debugpy.listen(('0.0.0.0', 12345))
-debugpy.wait_for_client()
+# import debugpy
+# debugpy.listen(('0.0.0.0', 12345))
+# debugpy.wait_for_client()
 output_image = model(prompt, num_inference_steps=30, generator=torch_g).images[0]
 output_image.save('out.png')
