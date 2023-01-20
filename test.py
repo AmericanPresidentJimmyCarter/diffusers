d03bcdbd10240915641abef3fa8139258d77ec84
Your Name <you@example.com>
2023-01-20 02:03:52 +0000

    WIP

---
+++ test.py
@@ -0,0 +1,26 @@
+from diffusers import StableDiffusionPipeline
+
+import torch
+import torch.nn.functional as F
+import utils
+from PIL import Image, ImageOps, ImageChops
+
+model = StableDiffusionPipeline.from_pretrained('stabilityai/stable-diffusion-2-1-base')
+from diffusers import EulerDiscreteScheduler
+
+THRESHOLD_FIRM = 190
+THRESHOLD_LOOSE = 60
+
+scheduler = EulerDiscreteScheduler.from_config(model.scheduler.config)
+model.scheduler = scheduler
+torch_g = torch.Generator(device='cuda')
+torch_g.manual_seed(12345)
+model = model.to('cuda')
+
+prompt = "pikachu surfing a giant wave eating an ice cream cone"
+
+import debugpy
+debugpy.listen(('0.0.0.0', 12345))
+debugpy.wait_for_client()
+output_image = model(prompt, num_inference_steps=30, generator=torch_g).images[0]
+output_image.save('out.png')
